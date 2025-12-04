b7cb33634711af501acb166a5d36de24d185cde2
Devaraju89 <149992893+Devaraju89@users.noreply.github.com>
2025-12-04 11:03:19 +0530

    updated App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="min-h-screen flex flex-col bg-gray-60">
         <Header />
 
         <main className="flex-1 p-6">
